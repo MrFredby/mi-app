b982ebb09f4c44259574151217864c81e602cb3a
MrFredby <fredyomarhernandezalba@gmail.com>
2026-04-10 11:51:54 -0600

    Mejora en la intertfaz de la pagina de rutaexpress

---
+++ src/services/api.ts
@@ -0,0 +1,5 @@
+export async function getTasks() {}
+export async function createTask() {}
+export async function updateTask() {}
+export async function deleteTask() {}
+export async function updateTaskStatus() {}
